3a551c7fed846a56730c2ddfa345d0bafe1690ee
Ted Neward <ted@tedneward.com>
2025-10-17 17:01:14 -0700

    Second commit

--- screen.py
+++ screen.py
@@ -4,14 +4,29 @@ import os
 import sys
 import xml.etree.ElementTree as ET
 
-print(os.getcwd())
+# First pass
 for d in os.listdir('./test'):
     if not os.path.exists(d):
         print("Examining directory:", d)
         xmlf = os.path.join('./test', d, 'test.xml')
         if os.path.exists(xmlf):
-            print(xmlf)
             tree = ET.parse(xmlf)
             root = tree.getroot()
             for desc in root.findall('description'):
                 print(desc.text)
+
+# Recursive search
+def searchdir(directory):
+    print("Searching directory:", directory)
+    for item in os.listdir(directory):
+        print("Examining item:", item)
+        if os.path.isdir(item):
+            searchdir(item)
+        elif item.endswith('.xml'):
+            print("Found XML file:", itempath)
+            tree = ET.parse(xmlf)
+            root = tree.getroot()
+            for desc in root.findall('description'):
+                print(desc.text)
+
+searchdir('./test')
